db887827aca7e6b121ba094a6bf75efd20c6c5ab
Carolina Siqueira <c.siqueiralima@gmail.com>
2017-03-28 21:19:32 -0300

    Finalização

---
+++ js/index.js
@@ -0,0 +1,69 @@
+$(function(){
+
+   $('#ghsubmitbtn').on('click', function(e){ 
+
+    e.preventDefault();
+    $('#ghapidata').html('<div align="center" id="loader"><img src="http://i.imgur.com/UqLN6nl.gif" alt="loading..."></div>');
+    
+    var username = $('#ghusername').val();
+    var requri   = 'https://api.github.com/users/'+username;
+    var repouri  = 'https://api.github.com/users/'+username+'/repos';
+    
+
+    requestJSON(requri, function(json) {
+      if(json.message == "Not Found" || username == '') {
+        $('#ghapidata').html("<h2>Usuario não encontrado!</h2>");
+      }
+      
+      else {
+        var fullname   = json.name;
+        var email      = json.email;
+        var username   = json.login;
+        var aviurl     = json.avatar_url;
+        var profileurl = json.html_url;
+        var location   = json.location;
+        var followersnum = json.followers;
+        var followingnum = json.following;
+        var reposnum     = json.public_repos;
+        var bio          = json.bio;
+        var created_at   = json.created_at;
+        
+        if(fullname == undefined) { fullname = username; }
+        
+        var outhtml = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
+        outhtml = outhtml + '<h2>'+email+'</h2><br></br>'
+        outhtml = outhtml + '<h2>'+bio+'</h2><br></br>'
+        outhtml = outhtml + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username+'"></a></div>';
+        outhtml = outhtml + '<p>Followers: '+followersnum+' - Following: '+followingnum+'<br>Repos: '+reposnum+'</p></div>';
+        outhtml = outhtml + '<div class="repolist clearfix">';
+        
+        var repositories;
+        $.getJSON(repouri, function(json){
+          repositories = json;   
+          outputPageContent();                
+        });          
+        
+        function outputPageContent() {
+          if(repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p></div>'; }
+          else {
+            outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
+            $.each(repositories, function(index) {
+              outhtml = outhtml + '<li><a href="'+repositories[index].html_url+'" target="_blank">'+repositories[index].name + '</a></li>';
+            });
+            outhtml = outhtml + '</ul></div>'; 
+          }
+          $('#ghapidata').html(outhtml);
+        } // end outputPageContent()
+      } // end else statement
+    }); // end requestJSON Ajax call
+  }); // end click event handler
+  
+  function requestJSON(url, callback) {
+    $.ajax({
+      url: url,
+      complete: function(xhr) {
+        callback.call(null, xhr.responseJSON);
+      }
+    });
+  }
+});
